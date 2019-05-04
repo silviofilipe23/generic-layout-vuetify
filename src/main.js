5e70ceb2ef2f0c68bf02bbabb58350098b6f1fee
Silvio Filipe <silvio.dionizio23@gmail.com>
2019-05-04 03:58:49 -0300

    Modificações na tela de profile

--- src/main.js
+++ src/main.js
@@ -6,52 +6,72 @@ import Vuetify from 'vuetify';
 import axios from 'axios';
 import LoginLayout from './Layouts/Login.vue';
 import DashboardLayout from './Layouts/Dashboard.vue';
+import VuetifyToast from 'vuetify-toast-snackbar';
 
 import 'vuetify/dist/vuetify.min.css';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
-  theme: {
-    primary: '#01579B',
-    secondary: '#039BE5',
-    accent: '#8c9eff',
-    error: '#b71c1c'
-  }
+    iconfont: 'md',
+    theme: {
+        primary: '#01579B',
+        secondary: '#039BE5',
+        accent: '#8c9eff',
+        error: '#b71c1c'
+    }
 });
 
+Vue.use(VuetifyToast, {
+    x: 'right', // default
+    y: 'top', // default
+    color: 'info', // default
+    icon: 'info',
+    timeout: 3000, // default
+    dismissable: true, // default
+    autoHeight: false, // default
+    multiLine: false, // default
+    vertical: false, // default
+    queueable: false, // default
+    shorts: {
+        custom: {
+            color: 'purple'
+        }
+    },
+    property: '$toast' // default
+})
+
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use(function(config) {
 
-  // const baseURL = 'URL_API';
-  const baseURL = 'https://portalapicloudid.stg.vaultid.com.br';
+    // const baseURL = 'URL_API';
+    const baseURL = 'https://portalapicloudid.stg.vaultid.com.br';
 
-  if (config.url.includes('http') === false) {
-    const newUrlPath = baseURL + config.url;
-    config.url = newUrlPath;
-  }
-  const token = localStorage.getItem('access_token');
-  if (token && config.url !== baseURL + '/oauth') {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, function (error) {
-  return Promise.reject(error);
+    if (config.url.includes('http') === false) {
+        const newUrlPath = baseURL + config.url;
+        config.url = newUrlPath;
+    }
+    const token = localStorage.getItem('access_token');
+    if (token && config.url !== baseURL + '/oauth') {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, function(error) {
+    return Promise.reject(error);
 });
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  if (response.status === 401 || response.status === 403) {
-    router.push('login');
-    location.reload();
-  }
-  return response;
-}, function (error) {
-  if (error.response.status === 401 || error.response.status === 403) {
-    router.push('login');
-  }
-  return Promise.reject(error);
+axios.interceptors.response.use(function(response) {
+    if (response.status === 401 || response.status === 403) {
+        router.push('login');
+        location.reload();
+    }
+    return response;
+}, function(error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+        router.push('login');
+    }
+    return Promise.reject(error);
 });
 
 Vue.component('login-layout', LoginLayout);
@@ -60,7 +80,7 @@ Vue.component('dashboard-layout', DashboardLayout);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app');
--- src/views/Perfil/Perfil.vue
+++ src/views/Perfil/Perfil.vue
@@ -17,27 +17,160 @@
     <v-flex xs12 sm6 md4 lg3 pa-2>
       <v-card class="pa-2">
         <v-toolbar flat color="transparent">
-          <v-toolbar-title>Silvio Filipe</v-toolbar-title>
+          <v-toolbar-title></v-toolbar-title>
+          <h1 font-weight-regular>Silvio Filipe Dionizio Junior</h1>
           <v-spacer></v-spacer>
-          <v-btn icon>
+          <v-btn icon @click="dialog = true">
             <v-icon>edit</v-icon>
           </v-btn>
         </v-toolbar>
-
+        <h2 class="font-weight-thin">silvio.dionizio23@gmail.com</h2>
         <h2 class="font-weight-thin">Rua 501, Qd. 02, Lt. 11, Casa 1</h2>
         <h2 class="font-weight-thin">74.917-300</h2>
         <h2 class="font-weight-thin">Jardim Mont-Serrat</h2>
         <h2 class="font-weight-thin">Aparecida de Goiania</h2>
       </v-card>
     </v-flex>
+    <v-dialog v-model="dialog" width="500" persistent>
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>Editar Usuário</v-card-title>
+        <v-card-text>
+          <v-form v-model="valid">
+            <v-container>
+              <v-layout>
+                <v-flex xs12>
+                  <v-text-field
+                    v-model="user.name"
+                    :rules="nameRules"
+                    :counter="50"
+                    label="Nome"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout>
+                <v-flex xs12>
+                  <v-text-field v-model="user.email" :rules="emailRules" label="E-mail" required></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout>
+                <v-flex xs6>
+                  <v-text-field
+                    v-model="user.cep"
+                    :rules="nameRules"
+                    :counter="8"
+                    label="Cep"
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs6>
+                  <v-btn round block color="primary" :disabled="!user.cep" @click="getCep">Buscar</v-btn>
+                </v-flex>
+              </v-layout>
+              <v-layout>
+                <v-flex xs12>
+                  <v-text-field
+                    v-model="user.logradouro"
+                    :rules="nameRules"
+                    label="Logradouro"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout>
+                <v-flex xs12 lg6>
+                  <v-text-field v-model="user.complemento" label="Complemento" required></v-text-field>
+                </v-flex>
+                <v-flex xs12 lg6>
+                  <v-text-field v-model="user.bairro" :counter="50" label="Bairro" required></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout>
+                <v-flex xs12 lg6>
+                  <v-text-field v-model="user.localidade" label="Cidade" required></v-text-field>
+                </v-flex>
+                <v-flex xs12 lg6>
+                  <v-text-field v-model="user.uf" :counter="2" label="UF" required></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error" flat @click="dialog = false">Cancelar</v-btn>
+          <v-btn color="primary" @click="submit">Salvar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-layout>
 </template>
 
 <script>
+const axios = require("axios");
+
 export default {
   data() {
-    return {};
+    return {
+      user: {
+        name: null,
+        email: null,
+        phone: null,
+        cep: null,
+        logradouro: null,
+        complemento: null,
+        bairro: null,
+        localidade: null,
+        uf: null
+      },
+      dialog: false,
+      valid: false,
+      nameRules: [
+        v => !!v || "Nome é obrigatório"
+        // v => v.length <= 40 || "O nome deve conter menos de 50 caracteres"
+      ],
+      email: "",
+      emailRules: [
+        v => !!v || "E-mail é obrigatório",
+        v => /.+@.+/.test(v) || "E-mail inválido"
+      ]
+    };
   },
-  components: {}
+  components: {},
+  methods: {
+    submit() {},
+    getCep() {
+      const self = this;
+      axios
+        .get("https://viacep.com.br/ws/" + self.user.cep + "/json/")
+        .then(function(response) {
+          if (response.data.erro) {
+            self.$toast.info("Cep não encontrado!", {
+              dismissable: true,
+              queueable: false,
+              timeout: 5000,
+              icon: "info"
+            });
+          } else {
+            self.user.logradouro = response.data.logradouro;
+            self.user.complemento = response.data.complemento;
+            self.user.bairro = response.data.bairro;
+            self.user.localidade = response.data.localidade;
+            self.user.uf = response.data.uf;
+          }
+        })
+        .catch(function(error) {});
+    },
+    saveUser() {
+      const self = this;
+    },
+    getUser() {
+      const self = this;
+    },
+    changePhoto(){
+      const self = this;
+    }
+  }
 };
 </script>
